2537a30c794cd91c942a7ba6366cb522974340a2
joseD <josed3000@gmail.com>
2023-09-26 18:19:39 +0200

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -17,6 +17,15 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <div className="container py-4 px-3 mx-auto">
+        <div className="col-6">
+          <h1>Hello, Bootstrap and Vite!</h1>
+        </div>
+        <div className="col-6">
+          {" "}
+          <button className="btn btn-primary">Primary button</button>
+        </div>
+      </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,7 +38,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
